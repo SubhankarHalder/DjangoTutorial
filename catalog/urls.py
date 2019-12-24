ccc0d0acdc8cf8b8c01588451e5bd7c6b8619938
Subhankar <subhankar@subhankar-pc.lan>
2019-12-24 15:59:46 +0530

    Sixth Part Tutorial

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     # Calls the view function that is a function named index() in views.py file 
     path('', views.index, name='index'),
+    # URL mapper for Book List View
+    path('books/', views.BookListView.as_view(), name='books'),
+    # URL mapper for Book Detail View
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
