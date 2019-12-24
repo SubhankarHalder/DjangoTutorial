7fefc9b7c9e032c3a592e3c81c3f8611b5bb80c9
Subhankar <subhankar@subhankar-pc.lan>
2019-12-24 15:24:36 +0530

    Fifth Part Tutorial

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,7 +13,7 @@ admin.site.register(Language)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death') 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-    
+     
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    # Calls the view function that is a function named index() in views.py file 
+    path('', views.index, name='index'),
 ]
